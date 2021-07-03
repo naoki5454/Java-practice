1c78d3be14f71383e0b27b08899e4d596f5bbef5
naoki5454 <naoking250912@icloud.com>
2021-07-03 15:12:25 +0000

    save

--- Main.java
+++ Main.java
@@ -4,3 +4,17 @@
 //         System.out.println("Hello world");
 //     }
 // }
+
+public class Main {
+  public static void main(String[] args) {
+    // 変数namesに、配列を代入してください
+    String[] names = {"にんじゃわんこ","ひつじ仙人","ベイビーわんこ"};
+    
+    // インデックス番号が0の要素を出力してください
+    System.out.println(names[0]);
+    
+    // インデックス番号が2の要素を出力してください
+    System.out.println(names[2]);
+    
+  }
+}
