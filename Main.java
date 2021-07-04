221f9aed5104d89a07d47ff29515c962e9112e35
naoki5454 <naoking250912@icloud.com>
2021-07-04 12:34:59 +0000

    save

--- Main.java
+++ Main.java
@@ -5,16 +5,28 @@
 //     }
 // }
 
-public class Main {
+// public class Main {
+//   public static void main(String[] args) {
+//     // 変数namesに、配列を代入してください
+//     String[] names = {"にんじゃわんこ","ひつじ仙人","ベイビーわんこ"};
+    
+//     // インデックス番号が0の要素を出力してください
+//     System.out.println(names[0]);
+    
+//     // インデックス番号が2の要素を出力してください
+//     System.out.println(names[2]);
+    
+//   }
+// }
+
+class Main {
   public static void main(String[] args) {
-    // 変数namesに、配列を代入してください
-    String[] names = {"にんじゃわんこ","ひつじ仙人","ベイビーわんこ"};
+    String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
     
-    // インデックス番号が0の要素を出力してください
-    System.out.println(names[0]);
-    
-    // インデックス番号が2の要素を出力してください
-    System.out.println(names[2]);
+    // for文を用いて、「私の名前は◯◯です」と出力してください
+    for(int i = 0; i < names.length; i++) {
+      System.out.println("私の名前は" + names[i] + "です");
+    }
     
   }
 }
