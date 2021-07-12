8826eb98033c1c056b5038b71a331f84d421d98b
naoki5454 <naoking250912@icloud.com>
2021-07-12 17:11:30 +0000

    java

--- Main.java
+++ Main.java
@@ -72,3 +72,16 @@
 //     System.out.println("Hello Java");
 //   }
 // }
+class Main {
+  public static void main(String[] args) {
+    // printDataメソッドを呼び出してください
+    printData();
+    
+  }
+  
+  // printDataメソッドを定義してください
+  public static void printData(){
+    System.out.println("私の名前はKate Jonesです");
+  }
+  
+}
